ffe35f8940d74bb7b695769f1c0dd538bbfcb1b0
TimS <tim.spencer95@hotmail.com>
2016-11-16 12:27:08 +0000

    make Employee and Stock entities

--- src/main/java/jar/Employee.java
+++ src/main/java/jar/Employee.java
@@ -1,9 +1,24 @@
 package jar;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.internal.NotNull;
+
 public class Employee {
 	//Variables
-	int id; 
-	String password_hash;
+	@Id
+	@Column(name="id", nullable =false, unique=true)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable=false, length=45)
+	@Size(max=45)
+	@NotNull
+	private String password_hash;
 	
 	//Employee constructor
 	public Employee (int id, String password_hash) {
