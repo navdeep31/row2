52d3813fb8f5cbd10bde580a3e661d9a193cb3c4
xeyali <adjduk@gmail.com>
2016-11-16 11:21:38 +0000

    Updated CustomerOrderLine and CustomerOrder

--- src/main/java/jar/CustomerOrder.java
+++ src/main/java/jar/CustomerOrder.java
@@ -1,27 +1,58 @@
 package jar;
 
+import java.util.*;
+
+/**
+ * Represents a customer order, using a list of customer order lines
+ * 
+ * @author Alex Dawson
+ * @version 0.2 16/11/2016
+ *
+ */
 public class CustomerOrder {
+	
+	/////////////////////////////////////////////FIELDS//////////////////////////////////////////////////
 	
 	private int id;
 	private List<CustomerOrderLine> orderLines;
 	private Address deliveryAddress;
 	private Address billingAddress;
 	
-	private static int idCount = 1;
+	private static int idCount = 0;
 	
+	/////////////////////////////////////////CONSTRUCTORS///////////////////////////////////////////////
+	
+	/**
+	 * Default constructor
+	 */
 	CustomerOrder()
 	{
-		
+		//TODO Keep this or not?
 	}
 	
+	/**
+	 * Constructor without id. Automatically sets id as next idCount
+	 * 
+	 * @param iOrderLines List of Order Lines
+	 * @param iDeliveryAddress Delivery Address (Address Object)
+	 * @param iBillingAddress Billing Address (Address Object)
+	 */
 	CustomerOrder(List<CustomerOrderLine> iOrderLines, Address iDeliveryAddress, Address iBillingAddress)
 	{
-		this.id = idCount++;
+		this.id = ++idCount;
 		this.orderLines = iOrderLines;
 		this.deliveryAddress = iDeliveryAddress;
 		this.billingAddress = iBillingAddress;
 	}
 	
+	/**
+	 * Constructor with id.
+	 * 
+	 * @param iId Customer Order ID
+	 * @param iOrderLines List of Order Lines
+	 * @param iDeliveryAddress Delivery Address (Address Object)
+	 * @param iBillingAddress Billing Address (Address Object)
+	 */
 	CustomerOrder(int iId, List<CustomerOrderLine> iOrderLines, Address iDeliveryAddress, Address iBillingAddress)
 	{
 		this.id = iId;
@@ -30,6 +61,8 @@ public class CustomerOrder {
 		this.deliveryAddress = iDeliveryAddress;
 		this.billingAddress = iBillingAddress;
 	}
+
+	////////////////////////////////////////METHODS/////////////////////////////////////////////////////
 
 	public int getId() {
 		return id;
