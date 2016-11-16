26eaa4899dfd8323b03c315b6777aa85fd7689bb
TimS <tim.spencer95@hotmail.com>
2016-11-16 11:08:17 +0000

    committing changes and adding wishlist

---
+++ src/main/java/jar/Wishlist.java
@@ -0,0 +1,12 @@
+package jar;
+
+public class Wishlist {
+	//Variables
+	int customer_id;
+	int product_id;
+	
+	//Wishlist constructor
+	public Wishlist(int customer_id, int product_id) {
+		
+	}
+}
