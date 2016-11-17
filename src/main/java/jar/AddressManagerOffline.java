afe5973612cc5aa7f99549196a31fad54866da6d
joshstan <joshuaethanstanley@gmail.com>
2016-11-17 14:30:24 +0000

    sorting out

---
+++ src/main/java/jar/AddressManagerOffline.java
@@ -0,0 +1,11 @@
+package jar;
+
+public class AddressManagerOffline implements AddressManager {
+
+	@Override
+	public void createAddress(Address address) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
