9d8e0ca99d9032c07f364f8c66b13d1cf7b3c6ac
xeyali <adjduk@gmail.com>
2016-11-22 12:57:08 +0000

    Added MethodAuthor Annotation in util

---
+++ src/main/java/com/bae/oc/util/MethodAuthor.java
@@ -0,0 +1,16 @@
+package com.bae.oc.util;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(CLASS)
+@Target({ METHOD, CONSTRUCTOR })
+public @interface MethodAuthor {
+	String author();
+}
