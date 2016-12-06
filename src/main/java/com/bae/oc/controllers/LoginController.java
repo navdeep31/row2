6dd77b704fed986ed7720158d56bf6fe75b25b0b
xeyali <adjduk@gmail.com>
2016-12-06 15:11:37 +0000

    Completed Login Controller

--- src/main/java/com/bae/oc/controllers/LoginController.java
+++ src/main/java/com/bae/oc/controllers/LoginController.java
@@ -7,35 +7,102 @@ import javax.inject.Named;
 import com.bae.oc.controllers.session.CurrentUser;
 import com.bae.oc.services.LoginService;
 
-@RequestScoped
-@Named(value="login")
-
 /**
+ * Controller handling login
  * 
  * @author Andrew Claybrook
- * @version 0.1 02/12/2016
+ * @author Alex Dawson
+ * @version 0.2 06/12/2016
  */
+@Named("login")
+@RequestScoped
 public class LoginController {
+	
 	@Inject
 	private CurrentUser currentUser;
 	@Inject
-	private LoginService login;
+	private LoginService loginService;
 	
 	private String email = "";
 	private String password = "";
 	
+	/**
+	 * Attempts login with non-empty email and password
+	 * 
+	 * Returns to homepage on completion
+	 * 
+	 * @return String of next page to visit
+	 * 
+	 * @MethodAuthor Andrew Claybrook
+	 * @MethodAuthor Alex Dawson
+	 */
 	public String login() {
-		if(!email.isEmpty() && !password.isEmpty())
-			if(login.validLogin(email, password))
-				currentUser.setCustomer(login.loginUser(email));
+		if(!email.isEmpty() && !password.isEmpty()){
+			if(loginService.validLogin(this.email, this.password)) {
+				currentUser.setCustomer(loginService.loginUser(email));
+				return "home";
+			}
+		}
 		return "home";
 	}
 	
+	/**
+	 * Logs user out (represented by setting to null)
+	 * 
+	 * Then sends to homepage
+	 * 
+	 * @return String of next page to visit
+	 * 
+	 * @MethodAuthor Andrew Claybrook
+	 * @MethodAuthor Alex Dawson
+	 */
 	public String logout() {
 		currentUser.setCustomer(null);
 		return "home";
 	}
 	
+	/**
+	 * Returns email
+	 * 
+	 * @return Email
+	 * 
+	 * @MethodAuthor Alex Dawson
+	 */
+	public String getEmail() {
+		return this.email;
+	}
+	
+	/**
+	 * Sets email
+	 * @param iEmail Email passed from form
+	 * 
+	 * @MethodAuthor Alex Dawson
+	 */
+	public void setEmail(String iEmail) {
+		this.email = iEmail;
+	}
+	
+	/**
+	 * Returns password
+	 * 
+	 * @return Password
+	 * 
+	 * @MethodAuthor Alex Dawson
+	 */
+	public String getPassword() {
+		return this.password;
+	}
+	
+	/**
+	 * Sets password
+	 * 
+	 * @param iPassword Password passed from form
+	 * 
+	 * @MethodAuthor Alex Dawson
+	 */
+	public void setPassword(String iPassword) {
+		this.password = iPassword;
+	}
 	
 
 }
