cefe1808c2f1153d2d17d0176095ef2b2d16d9f8
xeyali <adjduk@gmail.com>
2016-12-07 14:23:51 +0000

    Adding AddressService for validation and updating main xhtml template

---
+++ src/main/java/com/bae/oc/services/AddressService.java
@@ -0,0 +1,37 @@
+package com.bae.oc.services;
+
+import javax.ejb.Stateless;
+
+/**
+ * Deals with business logic for validating addresses
+ * 
+ * @author Alex Dawson
+ *
+ */
+@Stateless
+public class AddressService {
+	
+	/**
+	 * Checks if given address is valid
+	 * 
+	 * @param iLine1
+	 * @param iLine2
+	 * @param iPostcode
+	 * @param iCity
+	 * @return
+	 */
+	public boolean isValidAddress(String iLine1, String iLine2, String iPostcode, String iCity) {
+		
+		// invalid if no line1 or postcode
+		if(iLine1.isEmpty() || iPostcode.isEmpty()) {
+			return false;
+		}
+		
+		//TODO Validation for correct postcode format
+		
+		//TODO Validation for matching line1 and postcode
+		
+		return true;
+	}
+
+}
