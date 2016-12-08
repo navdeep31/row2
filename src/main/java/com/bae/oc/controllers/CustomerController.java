9819b073a5394aa15ea65d3b70252410b9a7348a
xeyali <adjduk@gmail.com>
2016-12-08 15:56:44 +0000

    Adding getset for CustController

--- src/main/java/com/bae/oc/controllers/CustomerController.java
+++ src/main/java/com/bae/oc/controllers/CustomerController.java
@@ -51,6 +51,8 @@ public class CustomerController {
 	 * Fills CustomerController attributes with Current User values when loading My Account Details Page
 	 * 
 	 * @return String of next page to visit (My Account Details)
+	 * 
+	 * @MethodAuthor Alex Dawson
 	 */
 	@PostConstruct
 	public void getCustomerDetails() {
@@ -64,6 +66,70 @@ public class CustomerController {
 			this.postcode = currentUser.getCustomer().getAddress().getPostcode();
 			this.city = currentUser.getCustomer().getAddress().getCity();
 		}
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLine1() {
+		return line1;
+	}
+
+	public void setLine1(String line1) {
+		this.line1 = line1;
+	}
+
+	public String getLine2() {
+		return line2;
+	}
+
+	public void setLine2(String line2) {
+		this.line2 = line2;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
 	}
 	
 }
