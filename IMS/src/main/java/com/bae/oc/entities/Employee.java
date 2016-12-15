52192d1da9a5079f59b19adafa04e79bf2c3a247
Tim Spencer <tim.spencer95@hotmail.com>
2016-12-15 09:47:33 +0000

    finishing off employee backend

--- IMS/src/main/java/com/bae/oc/entities/Employee.java
+++ IMS/src/main/java/com/bae/oc/entities/Employee.java
@@ -20,20 +20,34 @@ public class Employee {
 	@Id
 	@Column(name="id", nullable =false, unique=true)
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	private long id;
 	
 	@Column(nullable=false, length=45)
 	@Size(max=45)
 	@NotNull
 	private String password_hash;
 	
+	@Column(nullable=false)
+	private String email; 
+	
+	@Column(nullable=false)
+	private String name; 
+	
 	//Employee constructor
-	public Employee (int id, String password_hash) {
-		
+	public Employee (long id, String name, String email, String password_hash) {
+		this.id=id;
+		this.email=email;
+		this.password_hash=password_hash; 
+	}
+	
+	public Employee (String name, String email, String password_hash) {
+		id++; 
+		this.email=email;
+		this.password_hash=password_hash; 
 	}
 
 	//Getters and setters  
-	public int getId() {
+	public long getId() {
 		return id;
 	}
 
@@ -41,13 +55,32 @@ public class Employee {
 		return password_hash;
 	}
 
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
 	public void setPassword_hash(String password_hash) {
 		this.password_hash = password_hash;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 	
 	
 
--- IMS/src/main/java/com/bae/oc/util/TestData.java
+++ IMS/src/main/java/com/bae/oc/util/TestData.java
@@ -3,14 +3,28 @@ package com.bae.oc.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+import com.bae.oc.entities.Employee;
 import com.bae.oc.entities.Stock;
 import com.bae.oc.enums.ProductStatus;
 
-public class TestData {	{
+@Startup
+@Singleton
+public class TestData {
 
+	List<Stock> stock;
+	List<Employee> employees;
+	
+	@PostConstruct
+	private void setupData(){
+		
 	List<Stock> stock = new ArrayList<>();
-	
-	
+	List<Employee> employees = new ArrayList<>();
+		
+		
 	stock.add(new Stock(1l, ProductStatus.AVALIABLE));
 		stock.add(new Stock(2l, ProductStatus.AVALIABLE));
 		stock.add(new Stock(3l, ProductStatus.AVALIABLE));
@@ -24,5 +38,27 @@ public class TestData {	{
 		stock.add(new Stock(11l, ProductStatus.ON_HOLD));
 		stock.add(new Stock(12l, ProductStatus.AVALIABLE));
 		stock.add(new Stock(13l, ProductStatus.AVALIABLE));
-}
+
+	employees.add(new Employee(0, "Bill", "hello@email.com", "qwerty"));
+		employees.add(new Employee(1, "Al", "yes@email.com", "12345"));
+	
+	}
+
+	public List<Stock> getStock() {
+		return stock;
+	}
+
+	public void setStock(List<Stock> stock) {
+		this.stock = stock;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	
 }
